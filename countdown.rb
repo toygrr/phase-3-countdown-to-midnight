717ac68079890b2647fd6f63f00c9f58124e80b9
Tyler <toygrrr@gmail.com>
2022-08-29 09:37:07 -0700

    commit tree 57a330ba31f8ac3ff3d5550eca9057dada2170ec

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+def countdown x
+   while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+   end
+   "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep x
+    while x > 0
+        sleep(1)
+        puts "#{x} SECOND(S)!"
+        x -= 1
+       end
+       "HAPPY NEW YEAR!"
 end
